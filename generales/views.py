e303d86d480d0e6db69ed6aa72d82642d0f9e302
frpk21 <48810820+frpk21@users.noreply.github.com>
2024-03-11 13:23:01 -0500

    update

--- generales/views.py
+++ generales/views.py
@@ -161,7 +161,7 @@ def PublicacionesView(request):
     template_name = 'generales/public.html'
     hoy = date.today()
     categorias = Categoria.objects.all().order_by('id')
-    subcategorias = SubCategoria.objects.all().order_by('id')
+    subcategorias = SubCategoria.objects.filter(categoria__id=14)
     publicaciones = Noticias.objects.filter(subcategoria__categoria__id=14)
     c_p = Categoria.objects.get(id=14)
     context = {'hoy': hoy, 'cat_p': c_p, 'nosotros': Nosotros.objects.all().last(), 'publicaciones': publicaciones, 'categorias': categorias, 'subcategorias': subcategorias, 'modulos': SubCategoria.objects.filter(categoria__id=20).order_by('id')}
