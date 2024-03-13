2ab7c7d75173a761ec4c6ba9c617b46ee9bcc734
frpk21 <48810820+frpk21@users.noreply.github.com>
2024-03-13 14:09:52 -0500

    update

--- smt/urls.py
+++ smt/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('',include(('generales.urls','generales'), namespace='base')),
     path('admin/', admin.site.urls),
 ]
-#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
