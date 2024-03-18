daa106d9690c3c722d6783330fc59f5b8b7f7c83
frpk21 <48810820+frpk21@users.noreply.github.com>
2024-03-18 12:14:24 -0500

    visor

--- generales/views.py
+++ generales/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 
 from django.views import generic
 
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 
 from generales.models import Noticias, Comentario, Contacto, VideoSMT, Nosotros, Categoria, SubCategoria, Equipo
 
@@ -16,7 +16,23 @@ from generales.forms import SuscribirseForm, ComentarioForm, ContactoForm
 
 from django.http import JsonResponse
 from datetime import timedelta
-
+import json 
+import os
+import folium
+from folium.features import CustomIcon
+from folium.plugins import Search,  MarkerCluster, FastMarkerCluster
+from folium import plugins
+from geoserver.catalog import Catalog
+from django.views import generic
+from owslib.wms import WebMapService
+from owslib.wfs import WebFeatureService
+from owslib.fes import *
+from owslib.etree import etree
+import json
+import geopandas as gpd
+import requests
+from pyproj import CRS
+import geojson
 
 
 def HomeView(request):
@@ -389,3 +405,352 @@ class ContactView(generic.CreateView):
             )
         )
 
+
+def create_wfs(url):
+    """
+    Function that connect to GeoServer and return a WFS/OWS service
+    
+    Args:
+        url: A string -> str
+    
+    Return:
+        A WFS/OWS service -> owslib.feature.wfs110.WebFeatureService_1_1_0 object
+    """
+    return WebFeatureService(url, version='2.0.0')
+
+def get_data(wfs, layer):
+    """
+    Function that returns a GeoDataFrame or GeoJSON of data layer
+    
+    Args:
+        wfs: A WFS/OWS service -> owslib.feature.wfs110.WebFeatureService_1_1_0 object
+        layer: A layer name -> str
+    
+    Return:
+        A GeoDataFrame or GeoJSON data layer -> DataFrame or Dict
+    
+
+    # Fetch data from WFS
+    r = wfs.getfeature(typename=layer, outputFormat='json')
+    
+    # Get geometry layer
+    json_ = json.loads(r.read())
+    geom = json_['features'][0]['geometry']['type']
+
+    if geom == 'Point':
+        # Get GeoDataFrame layer
+        data = get_gdf(layer)
+    else:
+        data = json_
+        
+    return data, geom
+
+    """
+    # Define parameters
+    service = 'WFS'
+    version = '2.0.0'
+    request = 'GetFeature'
+    outputFormat = 'json'
+
+    # Specify parameters (read data in json format).
+    params = dict(service=service,
+                  version=version,
+                  request=request,
+                  typeName=layer,
+                  outputFormat=outputFormat)
+
+    # Fetch geojson data from WFS using requests
+    r = requests.get(wfs, params=params)
+
+    # Fetch data from WFS
+    #r = wfs.getfeature(typename=layer, outputFormat='json')
+    
+   # Get geometry layer
+    json_ = json.loads(r.content)
+    geom = json_['features'][0]['geometry']['type']
+        
+    return json_, geom
+
+def get_gdf(layer):
+    """
+    Function that returns a GeoDataFrame of data layer
+    
+    Args:
+        wfs: A WFS/OWS service -> owslib.feature.wfs110.WebFeatureService_1_1_0 object
+        layer: A layer name -> str
+    
+    Return:
+        A GeoDataFrame of data layer -> DataFrame
+    """
+
+    # Define parameters
+    service = 'WFS'
+    version = '2.0.0'
+    request = 'GetFeature'
+    outputFormat = 'json'
+    wfs_url = 'https://smt-test.onic.org.co/geoserver/wfs?'
+
+    # Specify parameters (read data in json format).
+    params = dict(service=service,
+                  version=version,
+                  request=request,
+                  typeName=layer,
+                  outputFormat=outputFormat)
+
+    # Fetch data from WFS using requests
+    r = requests.get(wfs_url, params=params)
+
+    # Create GeoDataFrame from geojson
+    gdf = gpd.GeoDataFrame.from_features(geojson.loads(r.content))
+
+    # Define crs
+    gdf.crs = CRS.from_epsg(4686)
+    return gdf
+
+def get_point_map(gjson, gdf, name_layer, catalogo):
+    """
+    Function that returns a layer point map instance
+    
+    Args:
+        gdf: GeoDataFrame of point data layer -> DataFrame
+        name_layer: A layer name -> str
+    
+    Return:
+        A layer point map -> FoliumMap
+    """
+
+    # Create a map instance
+    mymap = folium.Map(
+        location=[4.668730, -72.100403],
+        zoom_start=6,
+        tiles='cartodbpositron', attributionControl = False)
+
+    fields = list(gjson['features'][0]['properties'].keys()) 
+
+    names = ""
+    vakues_list = []
+    for field in fields:
+        names += '<b>' + field + '</b>' + ':{}<br>'
+        innerlist = gdf[field].values.tolist()
+        vakues_list.append(innerlist)
+    
+    #print(fields_list[5])
+    
+    # Get x and y coordinates for each point
+    lat = gdf["geometry"].apply(lambda geom: geom.x)
+    lon = gdf["geometry"].apply(lambda geom: geom.y)
+    
+    # Create a list of coordinate pairs
+    #locations = list(zip(gdf["y"], gdf["x"]))
+
+    marker_cluster = MarkerCluster(
+        name=name_layer,
+        overlay=True,
+        control=False,
+        icon_create_function=None
+    )
+    
+    for k in range(len(lon)):
+        values = [item[k] for item in vakues_list]
+        location = lon[k], lat[k]
+        marker = folium.Marker(location=location)
+        popup = names.format(*values) 
+        #popup = '<b>lon</b>:{}<br><b>lon</b>:{}'.format(location[1], location[0])
+        folium.Popup(popup).add_to(marker)
+        marker_cluster.add_child(marker)
+    
+    marker_cluster.add_to(mymap)
+        
+    #print(type(gjson))
+    #popup = folium.GeoJsonPopup(
+    #    fields=names,
+    ##    #aliases=["Nombre", "Departamento"],
+    #    localize=True,
+    #    labels=True,
+    ##    style="""
+    ##        background-color: #F0EFEF;
+    ##    """
+    #)
+
+    #tooltip = folium.GeoJsonTooltip(
+    #    fields=names[:-1],
+    #    #aliases=["Nombre", "Departamento"],
+    #    localize=True,
+    #    #sticky=False,
+    #    #labels=False,
+    #    #style="""
+    #    #    background-color: #F0EFEF;
+    #    #    border: 1px solid black;
+    #    #    border-radius: 3px;
+    #    #    box-shadow: 1px;
+    #    #""",
+    #    max_width=600,
+    #)
+    
+    mapdata = folium.GeoJson(
+        gjson,
+        name = name_layer,
+        #tooltip=tooltip,
+        #popup=popup,
+        #highlight_function=lambda x: {"fillOpacity": 0.8},
+        zoom_on_click=True,
+        show=False
+    ).add_to(mymap)
+    
+    layers = catalogo
+    idx_layer = layers.index(name_layer)
+
+    if idx_layer == 0:
+        search_field = fields[2]  
+    elif idx_layer == 1:
+        search_field = fields[3]
+    elif idx_layer == 2:
+        search_field = fields[5]
+    elif idx_layer == 3:
+        search_field = fields[7]    
+    else: 
+        search_field = fields[0]
+
+    controlsearch = Search(
+        layer=mapdata,
+        geom_type='Point',
+        placeholder='Buscador capa ' + name_layer,
+        cllapsed=False,
+        search_label=search_field,    
+    ).add_to(mymap)
+            
+    folium.LayerControl().add_to(mymap)
+    return mymap
+
+def get_polygon_map(gjson, name_layer, catalogo):
+    """
+    Function that returns a layer polygon map instance
+    
+    Args:
+        gdf: GeoJSON of polygon data layer -> geojson
+        name_layer: A layer name -> str
+    
+    Return:
+        A layer polygon map -> FoliumMap
+    """
+    
+    # Create a map instance
+
+    mymap = folium.Map(
+        location=[4.668730, -72.100403],
+        zoom_start=6,
+        titles='cartodbpositron', attributionControl = False)
+    
+    names = list(gjson['features'][0]['properties'].keys())    
+    popup = folium.GeoJsonPopup(
+        fields=names,
+        localize=True,
+        labels=True,
+    )
+
+    mapdata = folium.GeoJson(
+        gjson,
+        name = name_layer,
+        #tooltip=tooltip,
+        popup=popup,
+        highlight_function=lambda x: {"fillOpacity": 0.8},
+        zoom_on_click=True,
+        #popup_keep_highlighted=True
+    ).add_to(mymap)
+    #fg.add_child(mapdata)    
+    #mymap.add_child(fg)
+
+    layers = catalogo
+    idx_layer = layers.index(name_layer)
+
+    if idx_layer == 0:
+        search_field = names[2]  
+    elif idx_layer == 1:
+        search_field = names[3]
+    elif idx_layer == 2:
+        search_field = names[5]
+    elif idx_layer == 3:
+        search_field = names[7]    
+    else: 
+        search_field = names[0]
+
+    controlsearch = Search(
+        layer=mapdata,
+        geom_type='Polygon',
+        placeholder='Buscador capa ' + name_layer,
+        cllapsed=False,
+        search_label=search_field,    
+    ).add_to(mymap)
+
+    folium.LayerControl().add_to(mymap)
+
+    return mymap
+
+def format_iter(iterable, fmt='{!r}', sep=', '):
+    return sep.join(fmt.format(x) for x in iterable)
+
+class ajax_updateCapas(generic.View):
+    
+    def get(self, request):
+        capa = int(request.GET.get('capa', 0))
+        layer_name = request.GET.get('nombre', '')
+        wfs_url = 'https://smt-test.onic.org.co/geoserver/wfs?'
+
+        # Get data and geometry type layer
+        gjson_data, geom_type = get_data(wfs_url, layer_name)
+
+        
+        #capas
+        wfs_url = 'https://smt-test.onic.org.co/geoserver/wfs?'
+        wfs = create_wfs(wfs_url)
+        layers_catalogue = wfs.contents
+        catalogo=[]
+        for i, item in enumerate(layers_catalogue):
+            catalogo.append(item)
+            print("Capa # ", i, item)
+        
+        # Get map
+        if geom_type == 'Point':
+            gdf = gpd.GeoDataFrame.from_features(gjson_data)
+            gdf.crs = CRS.from_epsg(4686)
+            my_map = get_point_map(gjson_data, gdf, layer_name, catalogo)
+        else:
+            my_map = get_polygon_map(gjson_data, layer_name, catalogo)
+
+        map = my_map._repr_html_()
+
+        return JsonResponse(
+            {
+                'content': {
+                    'mapa': map
+                }
+            }
+        )
+    
+class Visor(LoginRequiredMixin, generic.TemplateView):
+    template_name='generales/home.html'
+    login_url='generales:login'
+
+    def get(self, request, *args, **kwargs):
+        lat = 4.668730
+        lon = -72.100403
+        logo_path = "static/base/image/favicon.png"
+        #logo = os.path.join(settings.BASE_DIR, logo_path)
+        m = folium.Map(location=[lat, lon], zoom_start=5, attributionControl = False)     
+        m = m._repr_html_()
+        wfs_url = 'https://smt-test.onic.org.co/geoserver/wfs?'
+        wfs = create_wfs(wfs_url)
+        layers_catalogue = wfs.contents
+        catalogo=[]
+        for i, item in enumerate(layers_catalogue):
+            catalogo.append(item) #[8:]
+        context = super().get_context_data(**kwargs)
+        context = {'catalogo': catalogo, 'mapa': m, 'l': len(layers_catalogue)}
+        #print("xxxxxxxxxxxxxxxxxxxxxxxxxxx ", type(layers_catalogue), layers_catalogue, catalogo)
+        
+        return self.render_to_response( 
+            self.get_context_data(
+                ctx=context,
+                mapa=m
+            )
+        )
