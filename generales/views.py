460772b960728f9811011974325aae3c7b400e7e
frpk21 <48810820+frpk21@users.noreply.github.com>
2024-03-18 08:58:43 -0500

    update

--- generales/views.py
+++ generales/views.py
@@ -152,8 +152,9 @@ def EquipoView(request):
     hoy = date.today()
     categorias = Categoria.objects.all().order_by('id')
     subcategorias = SubCategoria.objects.all().order_by('id')
-    equipo = Equipo.objects.all().order_by('orden')
-    context = {'hoy': hoy, 'nosotros': Nosotros.objects.all().last(), 'equipo': equipo, 'categorias': categorias, 'subcategorias': subcategorias, 'modulos': SubCategoria.objects.filter(categoria__id=20).order_by('id')}
+    equipo = Equipo.objects.all().exclude(orden=0).order_by('orden')
+    concejero = Equipo.objects.filter(orden=0).last()
+    context = {'hoy': hoy, 'concejero': concejero, 'nosotros': Nosotros.objects.all().last(), 'equipo': equipo, 'categorias': categorias, 'subcategorias': subcategorias, 'modulos': SubCategoria.objects.filter(categoria__id=20).order_by('id')}
 
     return render(request, template_name, context)
 
