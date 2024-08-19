e1f1d76e2418649c906b4a00007093a06229bd0a
frpk21 <48810820+frpk21@users.noreply.github.com>
2024-08-19 13:29:37 -0500

    banner 168px

--- generales/admin.py
+++ generales/admin.py
@@ -9,7 +9,7 @@ import random
 
 class NoticiasAdmin(admin.ModelAdmin):
 
-    list_display = ('titulo', 'subtitulo', 'fecha', 'orden', 'imagen', 'modificado', 'subcategoria', 'activo', )
+    list_display = ('titulo', 'subtitulo', 'fecha', 'orden', 'imagen', 'modificado', 'subcategoria_id', 'activo', )
     fields = ['subcategoria', 'titulo', 'subtitulo', 'fecha', ('orden', 'imagen'), 'descripcion', 'archivo_audio', 'fuente', 'html', 'pdf', 'activo']
     exclude = ('slug','autor', 'modificado', 'vistas',)
     ordering = ('orden', 'titulo', 'fecha',)
